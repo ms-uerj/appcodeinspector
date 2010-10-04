3e87f06e3388b3f6f23d6e30dbb1b9012ca8bd4e
HenriquePotter.hp <HenriquePotter.hp@58c12de5-09a6-932c-4b93-add33ed24f59>
2010-10-04 20:09:42 +0000

    Criado arquivo do dbml para uso do linqtoentities, desenvolvido métodos para inserção e autenticação de usuários usando camada DAO com FAÇADE(camada de negocio).

---
+++ WSCodeInspector/Database/FACADE/UsuarioFacade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Database.DAO;
+
+namespace Database.Facade
+{
+    public class UsuarioFacade
+    {
+        public bool AutenticarUsuario(string login, string senha)
+        {
+            return new UsuarioDAO().AtenticarUsuario(login,senha);
+        }
+
+        public bool CriarUsuario(string nome, string login, string email, string senha)
+        {
+            return new UsuarioDAO().CriarUsuario(nome, login, email, senha);
+        }
+    }
+}
