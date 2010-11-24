c9c7d880c429aff945c5b0fe2090bfcba51e8689
HenriquePotter.hp <HenriquePotter.hp@58c12de5-09a6-932c-4b93-add33ed24f59>
2010-11-24 03:34:46 +0000

    Criada classe de utilidades para o webservice, inicialmente possuindo o método de recuperacao de senha

---
+++ trunk/WSCodeInspector/WebServiceAcess/WebService/WebServiceUtil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+using CIFacade.Facade;
+using CIDao;
+
+namespace WebServiceAcess.WebService
+{
+    public class WebServiceUtil
+    {
+
+        public void EnviarSenhaToEmail(string toEmail)
+        {
+            try
+            {
+                Usuario usuario = new UsuarioFacade().RecuperarUsuario(toEmail);
+
+                StringBuilder emailBody = new StringBuilder();
+                emailBody.Append(" A senha cadastrada para este email é: ");
+                emailBody.Append(usuario.U_SENHA);
+
+                var fromAddress = new MailAddress("codeinspectorx@gmail.com", "CodeInspector");
+                var toAddress = new MailAddress(toEmail, usuario.U_NOME);
+                const string fromPassword = "nomorenations";
+                const string subject = "Recuperção de senha do CodeInspector";
+                string body = emailBody.ToString();
+
+                var smtp = new SmtpClient
+                           {
+                               Host = "smtp.gmail.com",
+                               Port = 587,
+                               EnableSsl = true,
+                               DeliveryMethod = SmtpDeliveryMethod.Network,
+                               UseDefaultCredentials = false,
+                               Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                           };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                        {
+                            Subject = subject,
+                            Body = body
+                        })
+                {
+                    smtp.Send(message);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
