f7c689d64831d24a8217c1359bf56171902c43b7
HenriquePotter.hp <HenriquePotter.hp@58c12de5-09a6-932c-4b93-add33ed24f59>
2010-12-02 03:57:34 +0000

    Adição da nova classe de acesso intermediario com a classe PartidaDAO camada DAO

---
+++ WSCodeInspector/CIFacade/Facade/PartidaFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CIDao.DAO;
+using CIDao;
+
+namespace CIFacade.Facade
+{
+    public class PartidaFacade
+    {
+
+        public void EncerrarPartida(int pontuacaoTotal,int nivelDificuldade,string userEmail)
+        {
+
+            #region [ Preenchimento do objeto ]
+
+            Partida partidaEncerrada = new Partida();
+
+            partidaEncerrada.P_NIVEL_DIFICULDADE = nivelDificuldade;
+            partidaEncerrada.P_PONTUACAO_TOTAL =  pontuacaoTotal;
+            partidaEncerrada.P_DATA = DateTime.Now;
+
+            #endregion
+
+            new PartidaDAO().EncerrarPartida(partidaEncerrada, userEmail);
+
+        }
+
+    }
+}
